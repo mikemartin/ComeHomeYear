428402178b1bdbd221dc839c429fad8798d40309
Alexis Georges <alexis.georges@gmail.com>
2012-04-03 21:42:12 -0700

    renamed env keys

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['COMING_HOME_FACEBOOK_KEY'], ENV['COMING_HOME_FACEBOOK_SECRET']
+  provider :facebook, ENV['COME_HOME_FACEBOOK_KEY'], ENV['COME_HOME_FACEBOOK_SECRET']
   provider :identity, :fields => [:email],
     :on_failed_registration => lambda { |env| IdentitiesController.action(:new).call(env) }
 end
