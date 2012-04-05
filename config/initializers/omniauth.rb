b54a144f11605970405fb215afdb229fad45301a
Alexis Georges <alexis.georges@gmail.com>
2012-04-05 08:40:27 -0700

    trying another client_option for omniauth-fb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['COME_HOME_FACEBOOK_KEY'], ENV['COME_HOME_FACEBOOK_SECRET'],
-    {:client_options => {:ssl => {:ca_file => '/etc/ssl/certs/ca-certificates.crt'}}}
+    {:client_options => {:ssl => {:ca_path => '/etc/ssl/certs'}}}
   provider :identity, :fields => [:email],
     :on_failed_registration => lambda { |env| IdentitiesController.action(:new).call(env) }
 end
