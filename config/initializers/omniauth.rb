ba8d3a2b51db72f74843f88c81113b9736018a17
Alexis Georges <alexis.georges@gmail.com>
2012-04-05 00:00:50 -0700

    added explicit reference to SSL certitifates for facebook

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['COME_HOME_FACEBOOK_KEY'], ENV['COME_HOME_FACEBOOK_SECRET']
+  provider :facebook, ENV['COME_HOME_FACEBOOK_KEY'], ENV['COME_HOME_FACEBOOK_SECRET'],
+    {:client_options => {:ssl => {:ca_file => '/etc/ssl/certs/ca-certificates.crt'}}}
   provider :identity, :fields => [:email],
     :on_failed_registration => lambda { |env| IdentitiesController.action(:new).call(env) }
 end
